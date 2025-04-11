c29b0beb73d34ec25fe3068fed008885a23236c8
pbohea <pbohea@gmail.com>
2025-04-11 15:15:56 +0000

    set user policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-    allow_browser versions: :modern
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  include Pundit
 
   before_action :authenticate_user!
   before_action :set_user_search, if: -> { current_user.present? }
@@ -13,9 +15,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :private, :name, :bio, :website, :avatar_image ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :private, :name, :bio, :website, :avatar_image ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :private, :name, :bio, :website, :avatar_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :private, :name, :bio, :website, :avatar_image])
   end
+
+  rescure_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
-  before_action :ensure_user_is_authorized, only [:show]
+  #before_action :ensure_user_is_authorized, only [:show]
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
@@ -9,6 +9,7 @@ class PhotosController < ApplicationController
 
   # GET /photos/1 or /photos/1.json
   def show
+    authorize @photo
   end
 
   # GET /photos/new
@@ -76,9 +77,9 @@ class PhotosController < ApplicationController
     end
   end
 
-  def ensure_user_is_authorized
-    if !PhotoPolicy.new(current_user, @photo).show?
-      redirect_back fallback_location: root_url
-    end
-  end
+  #def ensure_user_is_authorized
+  #  if !PhotoPolicy.new(current_user, @photo).show?
+  #   raise Pundit::NotAuthorizedError, "not allowed"
+  # end
+  #end
 end
